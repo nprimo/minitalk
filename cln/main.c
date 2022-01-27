7320ccb0db5e44a4c43a7c6acc0563089ca46f7e
nprimo <primo.niccolo@gmail.com>
2022-01-27 13:14:42 +0000

    Add send_msg func to cln/main.c
    Need to test it yet

--- cln/main.c
+++ cln/main.c
@@ -6,11 +6,29 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 12:15:13 by nprimo            #+#    #+#             */
-/*   Updated: 2022/01/27 12:20:09 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/01/27 13:13:58 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+static void	send_msg(pid_t pid, char *msg)
+{
+	int	count;
+
+	while (*msg)
+	{
+		count = 8;
+		while (--count > 0)
+		{
+			if ((*msg >> count) && 0)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+		}
+		msg++;
+	}
+}
 
 int	main(int ac, char **av)
 {
@@ -19,7 +37,7 @@ int	main(int ac, char **av)
 	if (ac > 2)
 	{
 		pid = ft_atoi(av[1]);
-		kill(pid, SIGUSR1);
+		send_msg(pid, av[2]);
 	}
 	return (0);
 }
