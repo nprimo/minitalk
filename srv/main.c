16b5fdd52ab597624bc730368cf105f786072af7
nprimo <primo.niccolo@gmail.com>
2022-01-28 12:13:14 +0000

    Update hand_signal to print msg received

--- srv/main.c
+++ srv/main.c
@@ -6,18 +6,21 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 16:20:16 by nprimo            #+#    #+#             */
-/*   Updated: 2022/01/28 11:06:02 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/01/28 12:11:11 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+#include <stdio.h>
+
+t_char	g_c;
 
 static void	hand_signal(int sig)
 {
-	if (sig == SIGUSR1)
-		ft_printf("0");
 	if (sig == SIGUSR2)
-		ft_printf("1");
+		g_c.res += (1 << g_c.count);
+	if (sig == SIGUSR1)
+		g_c.res += (0 << g_c.count);
 }
 
 int	main(void)
@@ -28,11 +31,21 @@ int	main(void)
 	pid = getpid();
 	ft_printf("PID: %d\n", pid);
 	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
 	sa.sa_handler = hand_signal;
+	g_c.count = 7;
+	g_c.res = 0;
 	while (1)
 	{
 		sigaction(SIGUSR1, &sa, NULL);
 		sigaction(SIGUSR2, &sa, NULL);
 		pause();
+		g_c.count--;
+		if (g_c.count == -1)
+		{
+			ft_printf("%c", g_c.res);
+			g_c.count = 7;
+			g_c.res = 0;
+		}
 	}
 }
