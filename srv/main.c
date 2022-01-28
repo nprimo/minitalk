16b706282fb3880a7d3a94879f996e93baf58af8
nprimo <primo.niccolo@gmail.com>
2022-01-28 12:42:25 +0000

    Update hand_signal to avoid use of global var

--- srv/main.c
+++ srv/main.c
@@ -6,21 +6,28 @@
 /*   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 16:20:16 by nprimo            #+#    #+#             */
-/*   Updated: 2022/01/28 12:11:11 by nprimo           ###   ########.fr       */
+/*   Updated: 2022/01/28 12:42:11 by nprimo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h>
 
-t_char	g_c;
-
 static void	hand_signal(int sig)
 {
+	static int	count = 7;
+	static char	c = 0;
+
 	if (sig == SIGUSR2)
-		g_c.res += (1 << g_c.count);
-	if (sig == SIGUSR1)
-		g_c.res += (0 << g_c.count);
+		c += (1 << count);
+	else if (sig == SIGUSR1)
+		c += (0 << count);
+	if (--count == -1)
+	{
+		ft_printf("%c", c);
+		count = 7;
+		c = 0;
+	}
 }
 
 int	main(void)
@@ -33,19 +40,10 @@ int	main(void)
 	sigemptyset(&sa.sa_mask);
 	sa.sa_flags = 0;
 	sa.sa_handler = hand_signal;
-	g_c.count = 7;
-	g_c.res = 0;
 	while (1)
 	{
 		sigaction(SIGUSR1, &sa, NULL);
 		sigaction(SIGUSR2, &sa, NULL);
 		pause();
-		g_c.count--;
-		if (g_c.count == -1)
-		{
-			ft_printf("%c", g_c.res);
-			g_c.count = 7;
-			g_c.res = 0;
-		}
 	}
 }
